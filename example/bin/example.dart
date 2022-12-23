b29f1c144c5444838fd8422942dadd3277c3ac41
Ngo Thanh Son <53457952+sonnts996@users.noreply.github.com>
2022-12-23 21:45:36 +0700

    initial

---
+++ example/bin/example.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+}
+
+int nonExpressFunction() {
+  return 0;
+}
+
+void iterableType() {
+  List hello = ['', 0, 0.1];
+  hello.whereType<int>();
+}
